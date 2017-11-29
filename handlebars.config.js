33086cbaad58d3248cbe21654738686e9e7b3379
Mega <Aza.Noriega@gmail.com>
2017-11-29 14:30:19 +0500

    move from 1.0.17 to 2.0.1 yahbuiler

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,7 +1,20 @@
+// module.exports = {
+// 	raw: 'src/templates',
+// 	compiled: 'client/templates',
+// 	bundle: 'client/templates',
+// 	bundleFilename: 'index.js',
+// 	minify: true
+// };
+
 module.exports = {
-	raw: 'src/templates',
-	compiled: 'client/templates',
-	bundle: 'client/templates',
-	bundleFilename: 'index.js',
-	minify: true
-};
+  entry: {
+    helpers: 'src/templates/helpers',
+    partials: 'src/templates/partials',
+    templates: 'src/templates/templates',
+  },
+  output: {
+    path: 'client/templates',
+    filename: 'index.js',
+    minify: true,
+  }
+}
